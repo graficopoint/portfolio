266ee2270851da6e605212016e43d91007b341ac
graficopoint <graficopoint@gmail.com>
2025-06-10 22:46:04 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: true, // Change this line to false disable auto-refreshing.
+  }
+})
